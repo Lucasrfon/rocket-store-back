54e569a673fb66030946d7978a730b73536eab9b
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-06 15:26:36 -0300

    inicio do projeto

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.listen(process.env.PORT, () => console.log(
+    `Server running on port ${process.env.PORT}.`
+));
