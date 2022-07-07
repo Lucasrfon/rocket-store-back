4eed19c2f417c92baf1acfc03b57f206ea4d776f
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-07 14:43:56 -0300

    teste

--- src/controllers/pokeController.js
+++ src/controllers/pokeController.js
@@ -5,13 +5,9 @@ export async function getPokemon (req, res){
     try {
         const pokemons = await db.collection('pokeCollection').find().toArray();
         res.status(201).send(pokemons);
-<<<<<<< HEAD
     } catch(error) {
         console.error(error);
-=======
-    } catch (error){
         res.sendStatus(500);
->>>>>>> main
     }
 }
 
