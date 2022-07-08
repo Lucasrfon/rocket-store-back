648ea9e859e2a968738780381818f1affa99d270
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-08 04:39:27 -0300

    implementa validateAuth e cartController

---
+++ src/controllers/cartController.js
@@ -0,0 +1,11 @@
+import { db } from "../dataBase/mongo";
+
+export async function getCart(req, res) {
+    try {
+        const session = req.locals.session;
+        const cart = await db.collection('carts').find({email: session.email}).toArray();
+        res.status(200).send(cart);
+    } catch (error) {
+        res.status(417).send('Erro ao obter carrinho.');
+    }
+}
--- src/middlewares/validateAuths.js
+++ src/middlewares/validateAuths.js
@@ -44,3 +44,17 @@ export async function validateLogin(req, res, next) {
     res.locals.validEmail = validEmail;
     next();
 }
+
+export default async function validateAuth(req, res, next) {
+    const { authorization } = req.headers;
+
+  const token = authorization?.replace('Bearer ', '');
+  const session = await db.collection('sessions').findOne({ token });
+
+  if (!session) {
+    return res.sendStatus(401);
+  }
+
+  res.locals.session = session;
+  next();
+}
