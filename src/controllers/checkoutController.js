8c3da4e36e9644a38eb963d302309cc29aa640ab
Lucas Fonseca <lucasrfon@gmail.com>
2022-07-11 17:32:31 -0300

    ajusta post checkout

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -2,18 +2,18 @@ import { db, ObjectId } from "../dbStrategy/mongo.js";
 
 export default async function postCheckout (req, res){
     try {
+        const session = res.locals.session;
         const {name, card, cvv, valid} = req.body;
         const cart = await db.collection('cart').findOne({email: session.email});
         await db.collection('pokeBuys').insertOne({
 
             cart: cart,
-            name: req.body.name,
-            card: req.body.card,
-            cvv:req.body.cvv,
-            valid:req.body.valid
+            name: req.body.cardName,
+            card: req.body.cardNumber,
+            cvv:req.body.cardCode,
+            valid:req.body.cardValid
         });
-        console.log(res)
-    res.sendStatus(201)
+    res.sendStatus(201);
     } catch (error){
         res.sendStatus(500);
     }
--- src/routes/checkoutRouter.js
+++ src/routes/checkoutRouter.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import validateCheckout from '../middlewares/validateCheckout.js';
 import checkoutController from '../controllers/checkoutController.js';
-import { validateToken, validateLogin } from '../middlewares/validateAuths.js';
-import { validateNewCart } from '../middlewares/validateCart.js';
+import { validateToken } from '../middlewares/validateAuths.js';
 
 const router = Router();
 
-router.post('/checkout', validateToken, validateLogin, validateCheckout, validateNewCart, checkoutController)
+router.post('/checkout', validateToken, validateCheckout, checkoutController)
 
 export default router;
