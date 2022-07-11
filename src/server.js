758d3b5f9570320c47379be7613115cb362513b5
Gabriel Rak Zanchetta <53123935+grakzanchetta@users.noreply.github.com>
2022-07-11 10:04:23 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 import authRouter from './routes/authRouter.js';
 import pokeRouter from './routes/pokeRouter.js';
 import cartRouter from './routes/cartRouter.js';
-<<<<<<< HEAD
+
 import checkoutRouter from './routes/checkoutRouter.js';
-=======
+
 import removeTokens from './middlewares/removeTokens.js';
->>>>>>> f397fda4b90b5a00c045554fa9666f6c84bd70cb
+
 
 dotenv.config();
 
