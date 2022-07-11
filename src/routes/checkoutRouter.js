bc6a914669e0ddd8e63464883696e0cf9bb71140
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-11 17:30:44 -0300

    fix autenticação compra

--- src/routes/checkoutRouter.js
+++ src/routes/checkoutRouter.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-
+import validateRegister from '../middlewares/validateCheckout.js';
 import checkoutController from '../controllers/checkoutController.js';
 
 const router = Router();
 
-router.post('/checkout', checkoutController)
+router.post('/checkout', validateRegister, checkoutController)
 
 export default router;
